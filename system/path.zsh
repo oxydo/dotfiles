f33429a89988a4a9b997c282ecb211e932a2998a
Kjeld Peters <ich@kjeld.de>
2015-07-22 15:35:44 +0200

    add 9.4 psql to path

--- system/path.zsh
+++ system/path.zsh
@@ -4,4 +4,8 @@ if [ -d "/Applications/Postgres.app/Contents/Versions/9.3/bin" ]; then
 	export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 fi
 
+if [ -d "/Applications/Postgres.app/Contents/Versions/9.4/bin" ]; then
+	export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
+fi
+
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
