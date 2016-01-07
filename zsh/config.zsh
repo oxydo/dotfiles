9fc5496ec3543883a004380c31f4e674c895b6a5
Kjeld Peters <ich@kjeld.de>
2016-01-07 10:58:23 +0100

    fix shortcuts after removal of boxen

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,13 +39,11 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey '^[b' backward-word
+bindkey '^[f' forward-word
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+bindkey "^[^?" backward-kill-word
 
 autoload -U select-word-style
 select-word-style bash
