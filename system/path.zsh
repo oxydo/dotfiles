51925d62734fc80a5f0c52540c57d28546ef8ddb
Kjeld Peters <ich@kjeld.de>
2016-06-06 16:36:17 +0200

    fix path to psql

--- system/path.zsh
+++ system/path.zsh
@@ -1,11 +1,7 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/bin/npm:$ZSH/bin:$HOME/.bin:$PATH"
 
-if [ -d "/Applications/Postgres.app/Contents/Versions/9.3/bin" ]; then
-	export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
-fi
-
-if [ -d "/Applications/Postgres.app/Contents/Versions/9.4/bin" ]; then
-	export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
+if [ -d "/Applications/Postgres.app/Contents/Versions/9.5/bin" ]; then
+	export PATH="/Applications/Postgres.app/Contents/Versions/9.5/bin:$PATH"
 fi
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
