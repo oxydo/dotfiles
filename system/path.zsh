b419f489c91ad35b149223fdd86112ee0fda4450
Kjeld Peters <ich@kjeld.de>
2017-07-21 09:58:28 +0200

    replace rvm by rbenv

--- system/path.zsh
+++ system/path.zsh
@@ -9,9 +9,26 @@ export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 # iterm2
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-# rvm
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
 # nvm stuff
 export NVM_DIR="$HOME/.nvm"
 test -e "/usr/local/opt/nvm/nvm.sh" && source "/usr/local/opt/nvm/nvm.sh"
+
+# rbenv
+export PATH="$HOME/.rbenv/shims:${PATH}"
+export RBENV_SHELL=zsh
+source '/usr/local/Cellar/rbenv/1.1.1/libexec/../completions/rbenv.zsh'
+command rbenv rehash 2>/dev/null
+rbenv() {
+  local command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(rbenv "sh-$command" "$@")";;
+  *)
+    command rbenv "$command" "$@";;
+  esac
+}
